bd8859922ccb7eebaccb71a4fd09765c94fe341a
naufalmuflih123 <112526698+naufalmuflih123@users.noreply.github.com>
2022-09-05 09:27:16 +0700

    Update main-sewa.js

--- plugins/main-sewa.js
+++ plugins/main-sewa.js
@@ -4,20 +4,19 @@ let handler = async (m, { conn }) => {
     pepe = 'https://i.ibb.co/BzR1jRq/08a088fe953b.jpg'
     baper = await fetch(pepe).then(a => a.buffer())
     let listMessage = {
-        "title": "◈ Made by Claraa",
+        "title": "◈ Made by Naufal Muflih",
         "description": `
 ┌「 *Sewa bot* 」
 │     
 ├ 1 Grup / 30 Hari
-├ Rp. 10,000 Gopay
-├ Rp. 15,000 Pulsa Three
+├ Rp. 20,000 Dana
+├ Rp. 20,000 Pulsa Indosat
 │
 ├ 1 Premium / 30 Hari
-├ Rp. 5,000 Gopay
-├ Rp. 10,000 Pulsa Three
+├ Rp. 10,000 Gopay
 │
 ├ tertarik? hubungi: 
-    ├ @62895604670507 (The.sad.boy01)
+    ├ @6289601124991 
 └────
 `.trim(),
         "listType": "PRODUCT_LIST",
